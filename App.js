2f7a3b6921ea82d9d65c7f552f4b4e06cfbb6a1d
miguels1lva <miguellemosdasilva2006@gmail.com>
2024-04-04 09:17:36 -0300

    first commit

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
-import Home from "./src/screens/Home";
+import { NavigationContainer } from "@react-navigation/native";
+import Routes from "./src/routes";
 
-export default function App(){
-  return (
-    <Home/>
-  );
+export default function App() {
+  return(
+    <NavigationContainer>
+    <Routes />
+    </NavigationContainer>
+  )
 }
---
+++ src/screens/Search.js
@@ -0,0 +1,9 @@
+import { View } from "react-native";
+
+export default function Search(){
+    return (
+    <View>
+        <Text>Busca</Text>
+    </View>
+    );
+}
